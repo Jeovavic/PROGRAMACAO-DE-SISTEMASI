8764b8e4434290884272e8f9dbb2f10d7484171b
Jeová Victor <109292622+Jeovavic@users.noreply.github.com>
2022-10-26 12:02:17 -0300

    Create Exercicio2.java

---
+++ src/Exercicio2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Exercicio2 {
+
+    public static void main(String[] args) {
+        /*Faça um programa em JAVA que solicite dois números ao usuárops calcule a média e exiba o resultado */
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Informe o primeiro número: ");
+        int n1 = scanner.nextInt();
+        System.out.println("Informe o segundo número: ");
+        int n2 = scanner.nextInt();
+
+        System.out.println("A média é: " + (n1 + n2)/2);
+    }
+    
+}
